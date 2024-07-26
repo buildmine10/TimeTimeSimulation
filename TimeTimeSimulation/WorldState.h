32c80a1cf6c1f744a4bd4e34956f9bfd42a1e7ac
buildmine10 <collin_stebbins@yahoo.com>
2024-07-26 17:15:07 -0400

    Started Setup

--- TimeTimeSimulation/WorldState.h
+++ TimeTimeSimulation/WorldState.h
@@ -2,8 +2,6 @@
 
 //A ledger of everything in space
 //This includes the state of the player, any physiscs object, etc.
-
-//Make a class that inherits from this class
 class WorldState
 {
 private:
