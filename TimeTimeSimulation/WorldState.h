c8d7c6609c204a1c4cc0f92e2cefa7e26bd93916
buildmine10 <collin_stebbins@yahoo.com>
2024-07-26 17:15:56 -0400

    Altered Comments

--- TimeTimeSimulation/WorldState.h
+++ TimeTimeSimulation/WorldState.h
@@ -2,15 +2,13 @@
 
 //A ledger of everything in space
 //This includes the state of the player, any physiscs object, etc.
-
-//Make a class that inherits from this class
 class WorldState
 {
 private:
 
 public:
-	virtual bool operator==(const WorldState& other) const;
+	bool operator==(const WorldState& other) const;
 
-	virtual WorldState update(double deltaTime) const;
+	WorldState update(double deltaTime) const;
 };
 
